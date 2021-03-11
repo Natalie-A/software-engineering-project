0b205280073ac550a32d03d47ee47250e215c8e6
Natalie-A <aoyanatalie40@gmail.com>
2021-03-11 19:37:47 +0300

    veru first commit

---
+++ login.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Login page</title>
+        <link rel="stylesheet" href="login.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+		<!-- <form action="processregister.php" method="post"> -->
+			<div class="body">
+				<h1>Login Page</h1>
+				<p>
+					<input type="email" name="email" placeholder="Email address" class="txt" required  autocomplete="off">
+				</p>
+				<p>
+					<input type="password" name="password" placeholder="Password" class="txt" required  autocomplete="off">
+				</p>
+				<p>
+					<button type="submit" name="submit" class="btn">Submit</button>
+				</p>
+			</div>
+		<!-- </form> -->
+	</body>
+</html>
---
+++ register.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Regsitration page</title>
+        <link rel="stylesheet" href="register.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+		<!-- <form action="processregister.php" method="post"> -->
+			<div class="body">
+				<h1>Registration Page</h1>
+				<p>
+					<input type="text" name="fullName" placeholder="Full Name" class="txt" required autocomplete="off">
+				</p>
+				<p>
+					<input type="email" name="email" placeholder="Email address" class="txt" required  autocomplete="off">
+				</p>
+				<p>
+					<input type="password" name="password" placeholder="Create Password" class="txt" required  autocomplete="off">
+				</p>
+				<p>
+					<button type="submit" name="submit" class="btn">Submit</button>
+				</p>
+			</div>
+		<!-- </form> -->
+	</body>
+</html>
