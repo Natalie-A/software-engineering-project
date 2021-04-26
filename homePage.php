7115e2950a0262a9ef8818838a2de88c7c643f5f
unknown <kamau.mercy@strathmore.edu>
2021-04-26 22:45:49 +0300

    google translate

--- homePage.php
+++ homePage.php
@@ -17,6 +17,15 @@ session_start();
         <div class="content">
             <header>
                 <a href="#" class="brand">Kenya</a>
+                <div id="google_translate_element"></div>
+                  <script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script><br>
+
                 <div class="menu-btn"></div>
                 <div class="navigation">
                   <div class="navigation-items">
--- util.php
+++ util.php
@@ -3,7 +3,7 @@
     {      
         static $DB_NAME = "e_id";       
         static $DB_USER = "root";        
-        static $DB_USER_PASS = "";        
+        static $DB_USER_PASS = "Memusi2020";        
         static $SERVER_NAME = "localhost";   
     }
 ?>
